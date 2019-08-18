134adbe676ec5db22609508eb33df3c323fffc86
Pelerin <infuzer@gmail.com>
2019-08-18 19:01:53 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 wsgi
