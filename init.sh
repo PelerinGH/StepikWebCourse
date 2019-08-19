2ee1b1f33b9ab0700a06fde8c472bf49bebe9a6d
Pelerin <infuzer@gmail.com>
2019-08-19 19:20:11 +0300

    2.1.11

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+#gunicorn -c --bind='0.0.0.0:8080' hello:app
+gunicorn -c --bind='0.0.0.0:8000' --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
