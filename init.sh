ba157b09fa140329d516fa64899974d2c1eff167
Ilia Klimov <infuzer@gmail.com>
2020-09-03 13:30:16 +0300

    update init files

--- init.sh
+++ init.sh
@@ -10,9 +10,7 @@ sudo apt install python3.5
 sudo apt install python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo python3 -m pip install gunicorn
-sudo python3 -m pip install django==2.0
-sudo python3 -m pip install mysqlclient
+sudo python3 -m pip install -r requirements.txt
 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database stepic_web;"
